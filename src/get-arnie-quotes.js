fd72c08f4c2e1fae61d4dd01a3f335ce4c9ed85b
Michael McHugh <michael@burningneuron.io>
2021-04-23 18:43:27 +1000

    Implement `getArnieQuotes()` stub function.
    Changes
    * Add constants to remove magic number dependencies
    * Add helper function parse response body
    * Add helper function request a single quote
    * Implement primary function to retrieve Arnie Quotes with error handling

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,45 @@
 const { httpGet } = require('./mock-http-interface');
 
+// Avoid magic values, set the response types here. Could be set via config in production.
+const SUCCESS = 'Arnie Quote';
+const FAILURE = 'FAILURE';
+
+/**
+ * Parses the response to ensure the body is an object
+ * @param {*} param0 
+ * @returns 
+ */
+const responseParser = ({ status, body }) => ({ status, body: JSON.parse(body) });
+
+/*
+  Returns a single object with an 'Arnie Quote' as the key
+  and the relevant quote.
+
+  Upon error, the object will have the key 'FAILURE' with the value
+  set to the error.
+*/
+const getArnieQuote = async (url) => {
+  const { body, status } = await httpGet(url).then(responseParser);
+  const key = status === 200 ? SUCCESS : FAILURE;
+  return {
+    [key]: body.message
+  };
+}
+
+/*
+  Returns a list of objects with "Arnie Quotes" or errors.
+  See `getArnieQuote()` for details.
+
+  This function assumes `urls` has already been validated to
+  be a list of url strings.
+*/
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const requests = urls.map(getArnieQuote);
+  const results = await Promise.all(requests);
+  return results;
 };
 
 module.exports = {
   getArnieQuotes,
+  getArnieQuote,
 };
